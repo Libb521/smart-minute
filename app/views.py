ca586ff0f1f01a25636a8ef2a816df8ba1a53fd1
Libb521 <eotieno39@yahoo.com>
2020-02-16 17:51:11 +0300

    added the title tag in the index.html file with an if statement, also added control blocks that were used to declare the if statement

--- app/views.py
+++ app/views.py
@@ -8,9 +8,9 @@ def index():
    '''
    View root page function that returns the index page and its data
    '''
-
+   title = 'Smart Minute Pitch'
    message = 'SMART MINUTE PITCH'
-   return render_template('index.html', message = message)
+   return render_template('index.html', message = message, title = title)
 
 @app.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):
--- run.py
+++ run.py
@@ -1,14 +1,3 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return '<h1>Smart Minute Pitch<h1>'
-
-# if __name__=='__main__':
-#     app.run(debug = True)
-
 from app import app
 
 if __name__ == '__main__':
