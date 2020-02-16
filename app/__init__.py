a9134ba756a1d8ebd0a99cc04ace42d824292fb6
Libb521 <eotieno39@yahoo.com>
2020-02-16 23:18:19 +0300

    installed the bootstrap extension and initialized the extension in the __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
 
 # Initializing application
+# bootstrap.init_app(app)
 app = Flask(__name__)
+
 
 #setting up configuration
 app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
