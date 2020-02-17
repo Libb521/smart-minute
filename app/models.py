8765545ac9c00801dc7b47003a8770e6bd664c16
Libb521 <eotieno39@yahoo.com>
2020-02-17 23:27:22 +0300

    I created a user model to define my user data

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
-import os
+# import os
 
-class Config:
+# class Config:
 
-   SECRET_KEY = os.environ.get('SECRET_KEY')
+#    SECRET_KEY = os.environ.get('SECRET_KEY')
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+      return f'User {self.name}'
