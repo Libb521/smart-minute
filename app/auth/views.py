7af8ef73b90143f65a3f5aa1a616681eaf4e5ae5
Libb521 <eotieno39@yahoo.com>
2020-02-18 23:57:42 +0300

    deployment to heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,18 +1,18 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
-from flask_login import login_user,logout_user,login_required
-
+from flask_login import login_user, logout_user, login_required
+from ..email import mail_message
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=LoginForm.email.data).first()
+        if user is not None and user.verify_password(LoginForm.password.data):
+            login_user(user, LoginForm.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Unacceptable username or Password')
