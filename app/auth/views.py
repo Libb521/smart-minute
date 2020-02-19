b969451439c58e7b7d3fc5d65a00b211e2a35aad
Libb521 <eotieno39@yahoo.com>
2020-02-19 06:21:17 +0300

    worked on restoring my navbar on the landing page, and making my sign in and sign up forms visible

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,17 +8,16 @@ from ..email import mail_message
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=LoginForm.email.data).first()
-        if user is not None and user.verify_password(LoginForm.password.data):
-            login_user(user, LoginForm.remember.data)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-
         flash('Unacceptable username or Password')
 
     title = "smart login"
-    return render_template('auth/login.html', login_form = login_form,title=title)
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route("/logout")
 @login_required
